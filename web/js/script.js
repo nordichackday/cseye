5dbda55a42238990973c16809c86977b0d84f785
Teemo Tebest <teemo.tebest@gmail.com>
2015-05-28 16:21:56 +0300

    Initial interface.

---
+++ web/js/script.js
@@ -0,0 +1,62 @@
+var app = {
+  formatNr: function (x, addComma) {
+    x = x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '&nbsp;');
+    x = x.replace('.', ',');
+    if (addComma === true && x.indexOf(',') === -1) {
+      x = x + ',0';
+    }
+    return (x == '') ? 0 : x;
+  },
+  roundNr: function (x, d) {
+    return parseFloat(x.toFixed(d));
+  },
+  setPath: function () {
+    if (location.href.match('http://yle.fi/plus/yle')) {
+      app.path = 'http://yle.fi/plus/yle/2015/' + app.projectName + '/';
+    }
+    else if (location.href.match('http://yle.fi')) {
+      app.path = 'http://yle.fi/plus/2015/' + app.projectName + '/';
+    }
+    else {
+      app.path = '2015/' + app.projectName + '/';
+    }
+  },
+  getScale: function () {
+    var width = $('#esi-vis').width();
+    if (width >= 578) {
+      $('#esi-vis').addClass('wide');
+      return true;
+    }
+    if (width < 578) {
+      $('#esi-vis').removeClass('wide');
+      return false;
+    }
+  },
+  initMediaUrls: function () {
+    $.each($('.handle_img', '#esi-vis'), function (i, el) {
+      $(this).attr('src', app.path + 'img/' + $(this).attr('data-src'));
+    });
+  },
+  initSortTable: function () {
+    var paging = false;
+    var searching = false;
+    // http://datatables.net/
+    $('.table_container', '#esi-vis').DataTable({
+      language: {
+        searchPlaceholder: 'Hae taulukosta',
+        url: yleApp.path + 'js/libs/Finnish.json'
+      },
+      order:[[2, 'asc']],
+      paging:paging,
+      searching:searching
+    });
+  },
+  init: function () {
+
+  }
+}
+
+
+$(document).ready(function () {
+  app.init();
+});
