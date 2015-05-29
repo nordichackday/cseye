11953b9418736eed40551768fcfbdea43332b528
Kjetil Mehl <kjetil.mehl@nrk.no>
2015-05-29 09:35:38 +0200

    Adds threading for udp server

--- groovy-parser/src/main/groovy/app/Application.groovy
+++ groovy-parser/src/main/groovy/app/Application.groovy
@@ -2,14 +2,20 @@ package app;
 
 import endpoint.UpdServer;
 import endpoint.WebAPI;
-import engine.GameEngine;
+import engine.GameEngine
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors;
 
 public class Application {
 
     public static void main(String[] args) {
-        GameEngine engine = new GameEngine()
-        WebAPI webAPI = new WebAPI(engine);
-        UpdServer upd = new UpdServer(engine);
+
+		GameEngine engine = new GameEngine()
+
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.execute(new UpdServer(engine))
+		WebAPI webAPI = new WebAPI(engine);
     }
 
 }
