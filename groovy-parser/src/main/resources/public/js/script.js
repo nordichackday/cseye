02c73540af478abcb4384ea2db30cd57d809149c
Jaakko Ojalehto <jaakko.ojalehto@yle.fi>
2015-05-29 12:43:38 +0300

    Fade in only the refreshed data cell

--- groovy-parser/src/main/resources/public/js/script.js
+++ groovy-parser/src/main/resources/public/js/script.js
@@ -57,39 +57,61 @@ var app = {
   },
   printDataWrapper: function (data) {
     app.printPlayerTable(data);
-    app.printRoundScoreStatistics(data);
-    app.printRoundStatistics(data);
-    app.printMessages(data);
+    //app.printRoundScoreStatistics(data);
+    //app.printRoundStatistics(data);
+    //app.printMessages(data);
+    app.data = data;
+  },
+  isChanged: function(previous, current, attribute) {
+    if(typeof previous !== 'undefined' && typeof previous[attribute] !== 'undefined' && typeof current !== 'undefined' && typeof current[attribute] !== 'undefined') {
+      if(previous[attribute] !== current[attribute]) {
+        return 'changed';
+      }
+      return '';
+    }
   },
   printPlayerTable: function(data) {
     var tbody = '';
+
     _.each(data.teams, function(team) {
+      var oldTeamIndex;
+      _.each(app.data.teams, function(oldTeam, index) {
+        if(team.id === oldTeam.id) {
+          oldTeamIndex = index;
+        }
+      });
       _.each(team.players, function(player) {
+        if(typeof oldTeamIndex !== 'undefined') {
+          var oldPlayer = _.find(app.data.teams[oldTeamIndex].players, function(oldPlayer) {
+            return player.id === oldPlayer.id;
+          });
+        }
         tbody += '<tr class="' + team.side + '"><td>' + player.name + '</td>';
-        tbody += '<td>' + player.kills + '</td>';
-        tbody += '<td>' + player.assists + '</td>';
-        tbody += '<td>' + player.deaths + '</td>';
-        tbody += '<td>';
+        tbody += '<td class="' + app.isChanged(oldPlayer, player, 'kills') + '">' + player.kills + '</td>';
+        tbody += '<td class="' + app.isChanged(oldPlayer, player, 'assists') + '">' + player.assists + '</td>';
+        tbody += '<td class="' + app.isChanged(oldPlayer, player, 'deaths') + '">' + player.deaths + '</td>';
+        tbody += '<td class="' + app.isChanged(oldPlayer, player, 'kills') + ' ' + app.isChanged(oldPlayer, player, 'deaths') + '">';
         if (player.deaths) {
-          tbody += app.roundNr(player.kills / player.deaths * 100, 1) / 100;
+          tbody += app.roundNr(player.kills / player.deaths * 100, 1);
         }
         tbody += '</td>';
-        tbody += '<td data-order="';
+        tbody += '<td class="' + app.isChanged(oldPlayer, player, 'kills') + ' ' + app.isChanged(oldPlayer, player, 'headshots') + '" data-order="';
         if (player.kills) {
           tbody += app.roundNr(player.headshots / player.kills * 100, 1);
         }
         tbody += '">';
         if (player.kills) {
-          tbody += app.roundNr(player.headshots / player.kills * 100, 1) / 1;
+          tbody += app.roundNr(player.headshots / player.kills * 100, 1);
         }
         tbody += ' %</td>';
-        tbody += '<td>' + player.points + '</td></tr>';
+        tbody += '<td class="' + app.isChanged(oldPlayer, player, 'points') + '">' + player.points + '</td></tr>';
       });
     });
     var table = '<table class="dataTable"><thead><tr><th class="sorting"><span>Name</span></th><th class="sorting"><span>Kills</span></th><th class="sorting"><span>Assists</span></th><th class="sorting"><span>Deaths</span></th><th class="sorting"><span>K/D Ratio</span></th><th class="sorting"><span>Headshot %</span></th><th class="sorting"><span>Score</span></th></tr></thead><tbody>' + tbody + '</tbody></table>';
     if (app.table != table) {
       app.table = table;
-      $('.player_table_container').html(app.table).fadeOut(0).fadeIn(1000);
+      $('.player_table_container').html(app.table);
+      $('.player_table_container .changed').fadeOut(0).fadeIn(5000);
       app.initSortTable();
     }
   },
@@ -178,6 +200,7 @@ var app = {
   },
   init: function () {
     app.timeOut = 0.1;
+    app.data = {};
     app.path = '';
     // app.initBars();
     app.initOwlCarousel();
